4f2c51a3fa2ff1c02fde6756b36e82bcbd26c6f8
Jula000 <128170039+Jula000@users.noreply.github.com>
2024-02-17 22:48:06 -0600

    init

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["media1.giphy.com", "images.pexels.com", "media2.giphy.com"],
+  },
+};
+
+module.exports = nextConfig;
